ce29ae6a4d8c001429e503f3b39848cd687ccb7a
Fernando Warno <ferwar@hotmail.com>
2024-12-22 23:10:06 -0300

    #matchers #toEqual #toBe

--- sum.test.js
+++ sum.test.js
@@ -1,7 +1,26 @@
 // ./sum.test.js
 const sum = require("./sum");
 
-it("should add 1 + 2 to equal 3", () => {
-    const result = sum(1, 2);
-    expect(result).toBe(3);
-}); 
+describe("example test", () => {
+
+    it("should add 1 + 2 to equal 3", () => {
+        const result = sum(1, 2);
+        expect(result).toBe(3);
+    });
+
+    it("object assignment", () => {
+        const obj = {}; // Creamos un objeto vacío y lo asignamos a la variable obj
+    
+        // Este primer test falla porque .toBe compara la referencia de los objetos.
+        // Aunque ambos son objetos vacíos, son instancias diferentes en memoria.
+        // Por lo tanto, obj y {} no son el mismo objeto, lo que hace que la comparación falle.
+        // expect(obj).toBe({}); // Falla: obj y {} son diferentes instancias.
+    
+        // Este segundo test pasa porque .toEqual compara el contenido de los objetos.
+        // Aunque obj y {} son diferentes instancias, ambos tienen las mismas propiedades
+        // (en este caso, ninguna), así que su contenido es considerado igual.
+        expect(obj).toEqual({}); // Pasa: obj y {} tienen el mismo contenido.
+    });
+    
+});
+
